0f2880ba5ac913e38482a58b198071431f8b2094
John Wright <jrite@protonmail.com>
2025-05-18 10:35:20 +0100

    fix: queued proxy

--- src/lib/server.ts
+++ src/lib/server.ts
@@ -6,14 +6,19 @@ export function serve(port: number, esmOrigin: string) {
   const proxy = httpProxy.createProxyServer()
 
   const server = createServer((req, res) => {
-    queue(async () => proxy.web(req, res, { target: esmOrigin })).catch(
-      (error) => {
-        res.statusCode = 500
-        res.setHeader('Content-Type', 'application/json; charset=utf-8')
-        res.write(JSON.stringify(error))
-        res.end()
-      },
-    )
+    queue(
+      () =>
+        new Promise<void>((resolve, reject) => {
+          res.on('error', reject)
+          res.on('finish', resolve)
+          proxy.web(req, res, { target: esmOrigin })
+        }),
+    ).catch((error) => {
+      res.statusCode = 500
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.write(JSON.stringify(error))
+      res.end()
+    })
   }).listen(port, () => {
     console.info('ESM proxy server listining on', server.address())
   })
